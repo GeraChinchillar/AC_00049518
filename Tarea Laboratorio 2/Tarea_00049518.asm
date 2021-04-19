4192d34d8c3bf9f4b9ae33bf8c9f9baef7094a62
Gerardo Chinchilla <00049518@uca.edu.sv>
2021-04-18 21:48:20 -0600

    add: Laboratorio 2

---
+++ Tarea Laboratorio 2/Tarea_00049518.asm
@@ -0,0 +1,28 @@
+; Christian Gerardo Chinchilla Ramirez
+; 00049518
+        
+        org     100
+
+        section .text
+
+        mov byte[200h], "C"
+        mov byte[201h], "G"
+        mov byte[202h], "C"
+        mov byte[203h], "R"
+
+        ; Direccionamiento Directo
+        mov AL, [200h]
+
+        ; Doreccionamiento indirecto por registro
+        mov BX, 201h
+        mov CL, [BX]
+
+        ; Direccionamiento indirecto base + indice
+        mov BX, 200h
+        mov SI, 0002h
+        mov DL, [BX + DI]
+
+        ; Direccionamiento relativo por registro
+        mov DI, [BX + 003h]
+
+
